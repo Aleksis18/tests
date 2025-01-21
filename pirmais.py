b52ce86b31e23c16128b1c04831e896d36b22ad0
Aleksis18 <guscis107@gmail.com>
2025-01-21 09:49:48 +0000

    v01

--- pirmais.py
+++ pirmais.py
@@ -1,1 +1,8 @@
-print()
+skaitlis = int(input("Ievadiet skaitli:"))
+print(type(skaitlis))
+if skaitlis>0:
+    print("Skaitlis pozitivs")
+elif skaitlis==0:
+    print("Skaitlis ir 0")
+else:
+    print("Skaitlis ir negatīvs")
