2e6488f467a8c90ea491ee4b9d89df3450497207
Aleksis18 <guscis107@gmail.com>
2025-01-28 10:15:14 +0000

    ja

---
+++ uzdevumi.py
@@ -0,0 +1,7 @@
+skaitlis1 = int(input("Ievadiet skaitli:"))
+skaitlis2 = int(input("Ievadiet skaitli:"))
+sk1_ped=skaitlis1%10
+sk2_ped=skaitlis2%10
+print(sk1_ped+sk2_ped)
+
+
---
+++ uzdevumi1.py
@@ -0,0 +1,4 @@
+skaitlis1 = int(input("Ievadiet naudu centos:"))
+centi=skaitlis1%100
+eiro=skaitlis1//100
+print(eiro, "eiro","un" ,centi ,"centu")
