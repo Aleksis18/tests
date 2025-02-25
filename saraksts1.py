124e2456efa284818173496edc6e01db7758bc6c
Aleksis18 <guscis107@gmail.com>
2025-02-25 10:21:03 +0000

    n

---
+++ saraksts1.py
@@ -0,0 +1,8 @@
+saraksts = ["Abols", "kivi", "mango", "bumbieris" , "banans"]
+saraksts.append("Persiks")
+saraksts.sort()
+print(saraksts)
+saraksts.remove(saraksts[1])
+print(saraksts[0])
+print(saraksts[-1])
+print(saraksts)
---
+++ saraksts2.py
@@ -0,0 +1,7 @@
+saraksts = ["1","2","3","4","5","6","7"]
+print(saraksts[0]," ",saraksts[-1])
+for i in range:
+    
+print(sum(saraksts))
+
+
